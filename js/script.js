70174048ffb2ebe3014ba13f439093b5998c3f79
santiago maximiliano bomba <sanmaximilianb@gmail.com>
2021-06-07 16:53:19 -0300

    Agregado Santiago

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-document.write("Robinson Alvarez, ")
-document.write("Merlo Osvaldo Matias")
-document.write(", Leopoldo Enrique MOLERO  MIRANDA, ")
-document.write("Ramiro Reynoso, ")
+document.write("Robinson Alvarez, ");
+document.write("Merlo Osvaldo Matias");
+document.write(", Leopoldo Enrique MOLERO  MIRANDA, ");
+document.write("Ramiro Reynoso, ");
+document.write("Santiago Maximiliano Bomba, ");
+
