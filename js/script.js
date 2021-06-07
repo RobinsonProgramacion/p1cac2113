b64bb3e4f82c904efe888c9820f8b713642b43c5
Javier Caballero <jm.caballero@gmail.com>
2021-06-07 20:02:14 -0300

    Add Javier Caballero

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ document.write("Merlo Osvaldo Matias");
 document.write(", Leopoldo Enrique MOLERO  MIRANDA, ");
 document.write("Ramiro Reynoso, ");
 document.write("Santiago Maximiliano Bomba, ");
+document.write("Javier Caballero, ");
 
